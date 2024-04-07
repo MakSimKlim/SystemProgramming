2ec02dcf234fb1e670bede5b2a716994f229b0df
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-07 23:19:28 +0500

    Add files via upload

---
+++ ДЗ №81 до 08.04.24 СистемноеПрогр_Процессы/ProcessesHomework2(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProcessesHomework2_WPF_
+{
+    public partial class MainWindow : Window
+    {
+        private Process process;
+        private DateTime startTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            process = new Process();
+            process.StartInfo.FileName = "E:/Родители/Папа/Системное программирование/ДЗ №81 до 08.04.24 СистемноеПрогр_Процессы/ProcessesHomework1/ProcessesHomework1/bin/Debug/ProcessesHomework1.exe";
+            process.EnableRaisingEvents = true;
+            process.Exited += Process_Exited;
+
+            startTime = DateTime.Now;
+            process.Start();
+        }
+
+        private void Process_Exited(object sender, EventArgs e)
+        {
+            this.Dispatcher.Invoke(() =>
+            {
+                TimeSpan runTime = DateTime.Now - startTime;
+                textBlock.Text = $"Приложение было запущено {runTime.TotalSeconds} секунд.";
+            });
+        }
+    }
+}
