6db3d69b0e0d7d3bfd99ae578ab18819e4023139
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-07 23:31:34 +0500

    Add files via upload

---
+++ ДЗ №82 до 11.04.24 СистемноеПрогр_Дочерние_Процессы/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace DaughterProcesses
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartNotepad_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("notepad.exe");
+        }
+
+        private void StartPaint_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("mspaint.exe");
+        }
+
+        private void StartCalculator_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("calc.exe");
+        }
+    }
+}
