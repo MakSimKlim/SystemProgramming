a01672ef8b9ad6d3c9d8a305356b56a6dc9df55d
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-07 23:18:36 +0500

    Add files via upload

---
+++ ДЗ №81 до 08.04.24 СистемноеПрогр_Процессы/ProcessesHomework1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        var processes = Process.GetProcesses();
+        var svchostProcesses = processes.Count(p => p.ProcessName.Contains("svchost"));
+        Console.WriteLine($"Количество процессов 'svchost': {svchostProcesses}");
+        Console.ReadKey();
+    }
+}
