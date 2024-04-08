59bf817d4955ba6895c8a964b1ffc76f11e03ed8
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-08 21:43:31 +0300

    Update MainWindow.xaml.cs

--- ДЗ №85 до 18.04.24 СистемноеПрогр_АсинхронВызов/MainWindow.xaml.cs
+++ ДЗ №85 до 18.04.24 СистемноеПрогр_АсинхронВызов/MainWindow.xaml.cs
@@ -13,17 +13,17 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
-        private void Button_Click(object sender, RoutedEventArgs e)                                         // Обработчик события нажатия на кнопку
+        private void Button_Click(object sender, RoutedEventArgs e)                        // Обработчик события нажатия на кнопку
         {
-            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();                      // Создаем диалоговое окно для выбора файла
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();     // Создаем диалоговое окно для выбора файла
 
-            Nullable<bool> result = dlg.ShowDialog();                                                       // Открываем диалоговое окно и получаем результат (true, если файл был выбран)
+            Nullable<bool> result = dlg.ShowDialog();                                      // Открываем диалоговое окно и получаем результат (true, если файл был выбран)
 
-            if (result == true)                                                                             // Если файл был выбран...
+            if (result == true)                                                            // Если файл был выбран...
             {
-                string filename = dlg.FileName;                                                             // Получаем путь к выбранному файлу
+                string filename = dlg.FileName;                                            // Получаем путь к выбранному файлу
 
-                FileStream fs = new FileStream(                                                            // Создаем поток для чтения файла
+                FileStream fs = new FileStream(                                            // Создаем поток для чтения файла
                     filename, 
                     FileMode.Open, 
                     FileAccess.Read, 
@@ -31,10 +31,10 @@ namespace WpfApp1
                     1024, 
                     FileOptions.Asynchronous);
 
-                new AsyncCallbackReader(fs, delegate (byte[] data)                                         // Создаем новый экземпляр AsyncCallbackReader для асинхронного чтения файла
+                new AsyncCallbackReader(fs, delegate (byte[] data)                         // Создаем новый экземпляр AsyncCallbackReader для асинхронного чтения файла
                 {
-                                                                                                          // Обновляем текстовый блок с содержимым файла
-                                                                                                          // Dispatcher.Invoke используется, чтобы обновить пользовательский интерфейс из другого потока
+                                                                                           // Обновляем текстовый блок с содержимым файла
+                                                                                           // Dispatcher.Invoke используется, чтобы обновить пользовательский интерфейс из другого потока
                     this.Dispatcher.Invoke(() =>
                     {
                         textBox.Text += $"============= {filename} =============\n{Encoding.UTF8.GetString(data)}\n";
@@ -43,27 +43,27 @@ namespace WpfApp1
             }
         }
     }
-    internal class AsyncCallbackReader                                                                   // Определяем класс AsyncCallbackReader для асинхронного чтения файла
+    internal class AsyncCallbackReader                                                     // Определяем класс AsyncCallbackReader для асинхронного чтения файла
     {
-        FileStream fs;                                                                                   // Объявляем необходимые поля
+        FileStream fs;                                                                     // Объявляем необходимые поля
         byte[] data;
         IAsyncResult aRes;
         AsyncBytesReadDel callbackMethod;
-        public AsyncCallbackReader(FileStream fs, AsyncBytesReadDel cbM)                                // Конструктор класса AsyncCallbackReader
+        public AsyncCallbackReader(FileStream fs, AsyncBytesReadDel cbM)                   // Конструктор класса AsyncCallbackReader
         {
-            this.fs = fs;                                                                               // Инициализируем поля
+            this.fs = fs;                                                                  // Инициализируем поля
             this.callbackMethod = cbM;
-            data = new byte[fs.Length];                                                                // создаем новый буфер data, размер которого равен размеру файла fs.Length
-                                                                                                       // Запускается асинхронная операция чтения из файла fs в буфер data. 
-            aRes = fs.BeginRead(data, 0, data.Length, ReadIsComplete, null);                           // Параметры 0 и data.Length указывают, что чтение должно начаться с начала буфера data и продолжаться до его конца, то есть, считываются все данные из файла
+            data = new byte[fs.Length];                                                    // создаем новый буфер data, размер которого равен размеру файла fs.Length
+                                                                                           // Запускается асинхронная операция чтения из файла fs в буфер data. 
+            aRes = fs.BeginRead(data, 0, data.Length, ReadIsComplete, null);               // Параметры 0 и data.Length указывают, что чтение должно начаться с начала буфера data и продолжаться до его конца, то есть, считываются все данные из файла
         }
-        public void ReadIsComplete(IAsyncResult res)                                                   // Callback-метод, который вызывается, когда чтение файла завершено
+        public void ReadIsComplete(IAsyncResult res)                                       // Callback-метод, который вызывается, когда чтение файла завершено
         {
-            int countByte = fs.EndRead(res);                                                           // Завершаем чтение файла
-            fs.Close();                                                                                // Закрываем поток
-            Array.Resize(ref data, countByte);                                                         // Изменяем размер массива данных, чтобы он соответствовал количеству прочитанных байт
-            callbackMethod(data);                                                                      // Вызываем callback-метод с данными из файла
+            int countByte = fs.EndRead(res);                                               // Завершаем чтение файла
+            fs.Close();                                                                    // Закрываем поток
+            Array.Resize(ref data, countByte);                                             // Изменяем размер массива данных, чтобы он соответствовал количеству прочитанных байт
+            callbackMethod(data);                                                          // Вызываем callback-метод с данными из файла
         }
     }
-    public delegate void AsyncBytesReadDel(byte[] streamData);                                         // Определяем делегат для callback-метода
+    public delegate void AsyncBytesReadDel(byte[] streamData);                             // Определяем делегат для callback-метода
 }
