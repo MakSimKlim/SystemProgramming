1322ada162b3399ad1b7366c8be869e9de55a21d
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-25 08:27:41 +0500

    Add files via upload

---
+++ ДЗ №87 до 25.04.24 СистемноеПрогр_DLL/Geometry.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GeometryLib
+{
+    public static class Geometry
+    {
+        //площадь квадрата
+        public static double SquareArea(double side)
+        {
+            return side * side;
+        }
+
+        //площадь прямоугольника
+        public static double RectangleArea(double length, double width)
+        {
+            return length * width;
+        }
+        //площадь треугольника
+        public static double TriangleArea(double basis, double height)
+        {
+            return 0.5 * basis * height;
+        }
+    }
+}
---
+++ ДЗ №87 до 25.04.24 СистемноеПрогр_DLL/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GeometryLib;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Площадь квадрата со стороной 5: " + Geometry.SquareArea(5));
+        Console.WriteLine("Площадь прямоугольника со сторонами 4 и 6: " + Geometry.RectangleArea(4, 6));
+        Console.WriteLine("Площадь треугольника с основанием 3 и высотой 7: " + Geometry.TriangleArea(3, 7));
+        Console.ReadKey();
+    }
+}
