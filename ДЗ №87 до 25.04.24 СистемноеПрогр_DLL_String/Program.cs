3e61fa3c3c311ed4f535c9f85b3e91f142023216
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2024-04-26 07:35:48 +0500

    Add files via upload

---
+++ ДЗ №87 до 25.04.24 СистемноеПрогр_DLL_String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using TextLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        //string text = "Madam Arora teaches malayalam. She was wearing a red colored dress. She felt Ill and was seeing blurred lines.";
+        string text = "А роза упала на лапу Азора.";
+
+        Console.WriteLine($"Text: {text}");                                  //текст палиндрома
+        Console.WriteLine($"Is Palindrome?: {text.IsPalindrome()}");         //проверка на палиндром
+        Console.WriteLine($"Word Count: {text.WordCount()}");                //подсчет количества слов
+        Console.WriteLine($"Sentence Count: {text.SentenceCount()}");        //подсчет количества предложений
+        Console.WriteLine($"Count of character 'а': {text.CharCount('а')}"); //подсчет количества опрееленных символов
+        Console.WriteLine($"Total character count: {text.CharCountAll()}");  //подсчет количества всех символов
+        Console.ReadKey(); 
+    }
+}
---
+++ ДЗ №87 до 25.04.24 СистемноеПрогр_DLL_String/StringExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextLibrary
+{
+    public static class StringExtensions
+    {
+        // проверка на палиндром
+        public static bool IsPalindrome(this string str)
+        {
+            var cleaned = new string(str.Where(char.IsLetter).ToArray()).ToLower();
+            return cleaned.SequenceEqual(cleaned.Reverse());
+        }
+        // подсчет количества слов в строке
+        public static int WordCount(this string str)
+        {
+            return str.Split(new[] { ' ', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+        //подсчет количества предложений в строке
+        public static int SentenceCount(this string str)
+        {
+            return Regex.Matches(str, @"[.!?]").Count;
+        }
+        //подсчет количества определенных символов в строке
+        public static int CharCount(this string str, char c)
+        {
+            return str.Count(ch => ch == c);
+        }
+        //подсчет количества всех символов в строке
+        public static int CharCountAll(this string str)
+        {
+            return str.Length;
+        }
+    }
+}
